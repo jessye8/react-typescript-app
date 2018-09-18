9cb78e693becbe163844b7d3aa98386686aa97df
jessye8 <jessye814@yahoo.com>
2018-09-18 09:32:03 -0700

    Update HeaderComponent.tsx

--- src/HeaderComponent.tsx
+++ src/HeaderComponent.tsx
@@ -1,15 +1,12 @@
 import * as React from "react";
 
-let Logo ="./img/ComingSoon.jpg";
-
 export default class FirstComponent extends React.Component <{}> {
         
         render() {
                 return (
                         <div>  
-                                <img src={Logo} />
-                                <h1>Hello, Welcome to the first page</h1>
+                               <h1>Hello, Welcome to the first page</h1>
                         </div>
-                );
+                       );
         }
 }
