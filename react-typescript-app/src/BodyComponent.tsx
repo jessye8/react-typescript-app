92fbd2733d83e8123342ff849d38efe28eda18aa
jessye8 <jessye814@yahoo.com>
2018-09-18 11:57:40 -0700

    Update BodyComponent.tsx

--- react-typescript-app/src/BodyComponent.tsx
+++ react-typescript-app/src/BodyComponent.tsx
@@ -7,7 +7,7 @@ export default class BodyComponent extends React.Component <{}> {
         render() {
                 return (
                         <div>  
-                            <img src={Logo}/> I am a component which shows a logo. For more information see the Readme.md at <a href='https://github.com/jessye8/react-typescript-app'>React-TypeScript-App</a>
+                            <img src={Logo}/> I am a component which shows a logo. For more information see the Readme.md at <a href='https://github.com/jessye8/starter-apps/react-typescript-app'>React-TypeScript-App</a>
                         </div>
                 );
         }
